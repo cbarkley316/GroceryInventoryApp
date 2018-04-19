b041c6ee40d70ef8440cf02efdec37965cf204a4
cbarkley316 <37199719+cbarkley316@users.noreply.github.com>
2018-04-19 14:05:12 -0700

    Update invTableViewCell.m

--- GroceryInventoryApp/invTableViewCell.m
+++ GroceryInventoryApp/invTableViewCell.m
@@ -5,6 +5,7 @@
 //  Created by Catherine Kreamer on 4/3/18.
 //  Copyright © 2018 Camden Barkley. All rights reserved.
 //
+// Did this transfer from Github?
 
 #import "invTableViewCell.h"
 #import "productCell.h"
